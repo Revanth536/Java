55d053211d3762ee5499ccb5cb363e97761b6373
Revanth536 <revanthrakesh181@gmail.com>
2026-02-12 10:13:44 +0530

    updated Prime file

--- Java/Prime.java
+++ Java/Prime.java
@@ -30,17 +30,35 @@ public class Prime {
         // }
 
         // Sum of prime numbers in a range
+        // int n = sc.nextInt();
+        // int n1 = sc.nextInt(), sum = 0;
+        // for (int i = n; i <= n1; i++) {
+        //     int fc = 0;
+        //     for (int j = 1; j <= i; j++) {
+        //         if (i % j == 0)
+        //             fc++;
+        //     }
+        //     if (fc == 2)
+        //         sum += i;
+            
+        // }System.out.println(sum);
+
+        // To find avernage of prime numbers in a range
         int n = sc.nextInt();
-        int n1 = sc.nextInt(), sum = 0;
+        int n1 = sc.nextInt(), sum = 0, count = 0;
         for (int i = n; i <= n1; i++) {
             int fc = 0;
             for (int j = 1; j <= i; j++) {
                 if (i % j == 0)
                     fc++;
             }
-            if (fc == 2)
+            if (fc == 2) {
                 sum += i;
-            
-        }System.out.println(sum);
+                count++;
+            }
+        }
+        System.out.println(sum+" "+count);
+        System.out.println(sum / count);
+        
     }
 }
