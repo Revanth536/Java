0321623a79661144feac51cb0c2a842a7a252c2b
Revanth536 <revanthrakesh181@gmail.com>
2026-02-12 09:40:06 +0530

    first code

---
+++ Java/Basic.java
@@ -0,0 +1,13 @@
+package Java;
+
+import java.util.*;
+
+class Basic {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
