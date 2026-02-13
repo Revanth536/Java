3a560335165964057eaf731f4959a5e828069280
Revanth536 <revanthrakesh181@gmail.com>
2026-02-13 19:13:27 +0530

    printing numbers with spaces

--- Java/Basic.java
+++ Java/Basic.java
@@ -6,8 +6,21 @@ class Basic {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a + b);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // System.out.println(a + b);
+
+        // Printing number with camma
+        int n = sc.nextInt();
+        int n1 = sc.nextInt();
+        int c = 0;
+        for (int i = n; i <= n1; i++) {
+            if (c==0) {
+                System.out.print(i + " ");
+                c++;
+            }
+            else
+                System.out.print(","+i);
+        }
     }
 }
