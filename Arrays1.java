5458431d7f7715ba245643e1d7cfc916402bdf00
Revanth536 <revanthrakesh181@gmail.com>
2026-02-15 09:00:34 +0530

    Arrays

---
+++ Arrays1.java
@@ -0,0 +1,24 @@
+// Reverse array
+
+// Find second largest
+
+// Remove duplicates
+
+// Rotate array
+
+// Two sum
+
+// Sort array
+
+import java.util.*;
+
+class Arrays1 {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++)
+            a[i] = sc.nextInt();
+        System.out.println(Arrays.toString(a));
+    }
+}
