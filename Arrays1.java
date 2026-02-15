509d1a57b6d0ba3a38c8d280b830ec93d4f4ca71
Revanth536 <revanthrakesh181@gmail.com>
2026-02-15 09:10:06 +0530

    other awy to reverse

--- Arrays1.java
+++ Arrays1.java
@@ -15,10 +15,21 @@ import java.util.*;
 class Arrays1 {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        int n = sc.nextInt();
-        int a[] = new int[n];
-        for(int i=0;i<n;i++)
-            a[i] = sc.nextInt();
+        // int n = sc.nextInt();
+        // int a[] = new int[n];
+        // for(int i=0;i<n;i++)
+        //     a[i] = sc.nextInt();
+        // System.out.println(Arrays.toString(a));
+
+        // Reverse Array
+        // int n = sc.nextInt();
+        // int a[] = new int[n];
+        // for(int i=0;i<n;i++)
+        //     a[i] = sc.nextInt();
+        // for(int i=a.length-1;i>=0;i--)
+        //     System.out.print(a[i]+" ");
+        Integer a[] = { 4, 5, 6, 7, 8 };
+        Collections.reverse(Arrays.asList(a));
         System.out.println(Arrays.toString(a));
     }
 }
