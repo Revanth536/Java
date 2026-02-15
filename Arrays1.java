949375c0308445731d33efdade3067cb388e2388
Revanth536 <revanthrakesh181@gmail.com>
2026-02-15 09:12:33 +0530

    Find second largest

--- Arrays1.java
+++ Arrays1.java
@@ -28,8 +28,16 @@ class Arrays1 {
         //     a[i] = sc.nextInt();
         // for(int i=a.length-1;i>=0;i--)
         //     System.out.print(a[i]+" ");
-        Integer a[] = { 4, 5, 6, 7, 8 };
-        Collections.reverse(Arrays.asList(a));
-        System.out.println(Arrays.toString(a));
+        // Integer a[] = { 4, 5, 6, 7, 8 };
+        // Collections.reverse(Arrays.asList(a));
+        // System.out.println(Arrays.toString(a));
+
+        // Find second largest
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++)
+            a[i] = sc.nextInt();
+        Arrays.sort(a);
+        System.out.println(a[a.length-2]);
     }
 }
