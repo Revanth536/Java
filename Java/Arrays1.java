b238f37bd2a919f7d8491c11e1d863f91b73aa34
Revanth536 <revanthrakesh181@gmail.com>
2026-02-15 09:30:10 +0530

    sorting

--- Java/Arrays1.java
+++ Java/Arrays1.java
@@ -16,6 +16,7 @@ import java.util.stream.Stream;
 
 class Arrays1 {
     static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         // int n = sc.nextInt();
         // int a[] = new int[n];
@@ -53,7 +54,22 @@ class Arrays1 {
         // Integer a[] = { 4, 5, 6, 7, 8, 4, 5 };
         // Set<Integer> set = new LinkedHashSet<>(Arrays.asList(a));
         // System.out.println(set);
-        
 
+        // Sort array
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+        // for (int i = 0; i < a.length - 1; i++) {
+        //     for (int j = 0; j < a.length - 1 - i; j++) {
+        //         if (a[j] > a[j + 1]) {
+        //             int t = a[j];
+        //             a[j] = a[(j + 1)] + a[j] - (a[j + 1] = a[j]);
+        //         }
+        //     }
+
+        // }
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
     }
 }
