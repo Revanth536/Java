d262a40254bd614c592a12fb8974ef599835f26f
Revanth536 <revanthrakesh181@gmail.com>
2026-02-17 11:41:23 +0530

    Amst in range

--- Java/Amst.java
+++ Java/Amst.java
@@ -5,24 +5,43 @@ import java.util.Scanner;
 public class Amst {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        int n = sc.nextInt();
-        int t = n,c=0,sum=0;
-        while (t > 0) {
-            c++;
-            t/=10;
-        }
-        t = n;
-        while (t > 0) {
-            int r = t % 10;
-            int pow = (int)Math.pow(r, c);
-            sum += pow;
-            t /= 10;
-        }
-        if(sum==n)
-            System.out.println("Armstrong");
-        else
-            System.out.println("Not an Armstrong");
+        // int n = sc.nextInt();
+        // int t = n,c=0,sum=0;
+        // while (t > 0) {
+        //     c++;
+        //     t/=10;
+        // }
+        // t = n;
+        // while (t > 0) {
+        //     int r = t % 10;
+        //     int pow = (int)Math.pow(r, c);
+        //     sum += pow;
+        //     t /= 10;
+        // }
+        // if(sum==n)
+        //     System.out.println("Armstrong");
+        // else
+        //     System.out.println("Not an Armstrong");
 
+
+         // Armstrong in Range
+        int n2 = sc.nextInt();
+        for (int i = n; i <= n2; i++) {
+            int t = i, c = 0, sum = 0;
+            while (t > 0) {
+                c++;
+                t /= 10;
+            }
+            t = i;
+            while (t > 0) {
+                int r = t % 10;
+                int pow = (int) Math.pow(r, c);
+                sum += pow;
+                t /= 10;
+            }
+            if(sum==i)
+                System.out.print(i+" ");
+        }
        
 
     }
