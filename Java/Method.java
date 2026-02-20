6bf3b66b43fad1464edacbc492401ba32a525cf9
Revanth536 <revanthrakesh181@gmail.com>
2026-02-20 15:14:02 +0530

    methods

--- Java/Method.java
+++ Java/Method.java
@@ -7,10 +7,32 @@ public class Method {
         return a + b;
     }
 
+    boolean Isprime(int n) {
+        int fc = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                fc++;
+            }
+        }
+        if(fc == 2) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         int n = sc.nextInt(), n1 = sc.nextInt();
         Method m = new Method();
-        System.out.println(m.add(n, n1));
+        // System.out.println(m.add(n, n1));
+        System.out.print(n + " is ");
+        if(m.Isprime(n)) {
+            System.out.print("a prime number");
+        }
+        else {
+            System.out.print("not a prime number");
+        }
 
     }
 }
