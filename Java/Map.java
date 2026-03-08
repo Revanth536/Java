d1108f23625b28c6a7346317dfd8ba1d65644522
Revanth536 <133359568+Revanth536@users.noreply.github.com>
2026-03-08 16:26:37 +0530

    Add phonebook solution using HashMap
    Implement a phonebook solution using HashMap to store names and phone numbers, and handle queries for phone numbers.

---
+++ Java/Map.java
@@ -0,0 +1,35 @@
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+// import java.BufferedReader;
+import java.io.BufferedReader;
+// import java.Exception;
+class Solution{
+	public static void main  (String []argh)throws Exception
+	{
+	 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.valueOf(br.readLine());
+        HashMap<String, Integer> map = new HashMap<>();
+        while (n-- > 0) {
+            String name = br.readLine();
+            int phone   = Integer.valueOf(br.readLine());
+            map.put(name, phone);
+        }
+        
+        /* Read each query and check if its in our HashMap */
+        String s;
+        while((s = br.readLine()) != null) {
+            if (map.containsKey(s)) {
+                System.out.println(s + "=" + map.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        
+        br.close();
+    }
+
+}
+
+
+
